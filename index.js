564430ad9d7c76a17e800e186615c81494738838
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-16 17:36:05 -0400

    added function outline to create & init

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
-        {
+const prompts = [{
         type: 'input',
         message: 'Enter 3 letters for your logo',
         name: 'text',
@@ -22,8 +20,18 @@ inquirer
         type: 'input',
         message: 'Enter a color keyword for your shape color',
         name: 'shapeColor',
-        },
-    ]);
+        }];
 
+function createSVG(data) {
 
+};
 
+function init() {
+    inquirer
+        .prompt(prompts)
+        .then(data => {
+            createSVG(data);
+        });
+};
+
+init();
