375601dd305617a04eacba1d99277e1db305404c
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-19 09:20:35 -0400

    updated x y for rendertext function

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ function createSVG(data) {
   
    const logoFrame =`
    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-   ${svgLogo.renderText()}
    ${svgLogo.renderTriangle()}
+   ${svgLogo.renderText()}
    </svg>
    `;
     
